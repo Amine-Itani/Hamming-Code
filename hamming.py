fc98282fbb5db48a1d8dcdb6f03638849ac14b01
Amine-Itani <123438294+Amine-Itani@users.noreply.github.com>
2024-09-04 09:43:34 +0900

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,1 +1,32 @@
 
+def position_parity(m, k):
+    n=''
+    count_m = 0
+    count_k = 0
+    for i in range(0, len(k)+len(m)):
+        if i+1 != 0 and (i+1 & (i)) == 0:
+            n += k[count_k]
+            print(k[count_k])
+            count_k += 1
+
+        else:
+            n += m[count_m]
+            print(m[count_m])
+            count_m += 1
+
+    return n
+
+# example test
+test_1 = position_parity('1011', ['P0','P1','P2'])
+print(test_1)
+
+def parity_maker(k,n,p):
+    par = []
+    m = k+n
+    for i in range (0, m+1):
+        if i & 2**(p-1):
+            par.append(i)
+    return par
+
+test1 = parity_maker(3,4,3)
+print(test1)
